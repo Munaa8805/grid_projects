cb4a861a49036252d116925dfd1dcf3389aef7e9
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2025-01-23 20:38:26 -0700

    change something

--- components/Grids.js
+++ components/Grids.js
@@ -74,26 +74,7 @@ const dataImages = [
 ];
 
 const Grids = () => {
-  useEffect(() => {
-    async function LoadImages() {
-       const response = await fetch(
-         `https://res.cloudinary.com/djws6cbsg/image/upload`,
-         {
-           headers: {
-             Authorization: `Basic ${Buffer.from(
-               process.env.CLOUDINARY_API_KEY +
-                 ":" +
-                 process.env.CLOUDINARY_API_SECRET
-             ).toString("base64")}`,
-           },
-         }
-       ).then((r) => r.json());
 
-    //    return response;
-    console.log("dara", response);
-    }
-    LoadImages();
-  }, []);
   return (
     <div className="p-5 grid grid-cols-2 md:p-0 md:grid-cols-3 gap-5">
       {dataImages.map((image) => (
